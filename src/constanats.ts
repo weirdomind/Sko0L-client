af4bb3eeaba87394213a87f5cc660aa8fe5a7b02
Mind0Bender <mrcircuit1234@gmail.com>
2022-04-03 14:18:23 +0530

    [FIX] SERVER_URL 1/2, deploy test [0*]

--- src/constanats.ts
+++ src/constanats.ts
@@ -1,1 +1,5 @@
-export const SERVER_URL = "http://localhost:8080";
+export const ISDEV = process.env.NODE_ENV === "development";
+export const SERVER_URL = ISDEV
+  ? "http://localhost:8080"
+  : "https://mysko0l-api.herokuapp.com";
+console.log(SERVER_URL);
