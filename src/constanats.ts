bf7a9cc106036ad0d778c46d002d65531e54f522
Mind0Bender <mrcircuit1234@gmail.com>
2022-04-04 14:57:36 +0530

    [FIX] react_app_server_url [0*]

--- src/constanats.ts
+++ src/constanats.ts
@@ -1,4 +1,4 @@
 export const ISDEV = process.env.NODE_ENV === "development";
 export const SERVER_URL = ISDEV
   ? "http://localhost:8080"
-  : process.env.SERVER_URL || process.env.REACT_APP_SERVER_URL;
+  : (process.env.SERVER_URL || process.env.REACT_APP_SERVER_URL)!;
