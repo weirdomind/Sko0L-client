3fbc13dd5a2661a0d77b43abb4c8819bee5d47a8
Mind0Bender <mrcircuit1234@gmail.com>
2022-04-05 18:07:25 +0530

    [FIX] react_app_server_url [0*]

--- src/constanats.ts
+++ src/constanats.ts
@@ -1,6 +1,3 @@
+console.log(process.env.NODE_ENV);
 export const ISDEV = process.env.NODE_ENV === "development";
-export const SERVER_URL = ISDEV
-  ? "http://localhost:8080"
-  : process.env.SERVER_URL || "https://mysko0l-api.herokuapp.com";
-
-console.log(SERVER_URL, process.env);
+export const SERVER_URL = "https://mysko0l-api.herokuapp.com";
