f23252287a81954b770307d5fb32ae6949b6ad81
Mind0Bender <mrcircuit1234@gmail.com>
2022-04-06 14:34:38 +0530

    [FIX] server url [0*]

--- src/constanats.ts
+++ src/constanats.ts
@@ -1,3 +1,5 @@
 console.log(process.env.NODE_ENV);
 export const ISDEV = process.env.NODE_ENV === "development";
-export const SERVER_URL = "https://mysko0l-api.herokuapp.com";
+export const SERVER_URL = ISDEV
+  ? "http://localhost:8080"
+  : "https://mysko0l-api.herokuapp.com";
